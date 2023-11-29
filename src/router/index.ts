cb2b916e1df06b78d1d90f7d1fd6816d31ee07b2
Vicgri <89450050+Vicgri@users.noreply.github.com>
2023-11-29 21:47:35 +0100

    added sign up

--- src/router/index.ts
+++ src/router/index.ts
@@ -5,7 +5,7 @@ import TabsPage from '../views/TabsPage.vue'
 const routes: Array<RouteRecordRaw> = [
   {
     path: '/',
-    redirect: '/tabs/gallery'
+    redirect: '/tabs/authentication'
   },
   {
     path: '/tabs/',
