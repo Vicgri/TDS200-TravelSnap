b58e3a2dcd0f0a6cea2bb1e2559baa464b43b377
Vicgri <89450050+Vicgri@users.noreply.github.com>
2023-12-08 17:42:24 +0100

    search fungerer

--- src/views/SearchView.vue
+++ src/views/SearchView.vue
@@ -9,13 +9,38 @@
         <input v-model="searchTerm" />
         <button @click="search">Search</button>
         <div v-if="searchResults.length">
-          <ul>
-            <li v-for="result in searchResults" :key="result.id">
-              <div v-for ="(value, field) in result">
-               {{ field }}:{{ value }}
-              </div>
-            </li>
-          </ul>
+
+          <ion-grid>
+            <ion-row>
+              <ion-col
+                  v-for="result in searchResults"
+                  :key="result.id"
+                  size="12"
+                  size-md="6"
+                  size-lg="4"
+              >
+                <ion-card>
+                  <GalleryComponent
+                      :id="result.id"
+                      :title="result.title"
+                      :image-urls="result.imageUrls"
+                  />
+                  <ion-chip-group>
+                    <ion-chip
+                        v-for="hashtag in result.hashtags"
+                        :key="hashtag"
+                        class="hashtag"
+                    >
+                      {{ hashtag }}
+                    </ion-chip>
+                  </ion-chip-group>
+                </ion-card>
+              </ion-col>
+            </ion-row>
+          </ion-grid>
+            <div v-for="result in searchResults" :key="result.id">
+              <hr />
+            </div>
         </div>
       </div>
     </ion-content>
@@ -23,18 +48,22 @@
 
 <script lang="ts">import { db } from "@/main";
 import { collection, query, where, getDocs } from "firebase/firestore"
+import { NewTravelSnap } from "@/models/TravelSnapModel";
+import {ref} from "vue/dist/vue";
+import GalleryComponent from "@/components/GalleryComponent.vue";
 
 export const getTravels = async (searchTerm: string) => {
   const q = query(collection(db, 'travel'), where('title', '==', searchTerm));
   const querySnapshot = await getDocs(q);
-  return querySnapshot.docs.map((doc) => doc.data());
+  return querySnapshot.docs.map((doc) => doc.data())as NewTravelSnap [];
 };
 
 export default {
+  components: {GalleryComponent},
   data() {
     return {
       searchTerm: '',
-      searchResults: [] as any[],
+      searchResults: [] as NewTravelSnap [],
     };
   },
   methods: {
