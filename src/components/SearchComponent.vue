e77d610bc6f339e17bbaaa008ea7d39162aee6bd
Vicgri <89450050+Vicgri@users.noreply.github.com>
2023-12-08 17:05:27 +0100

    search får opp liste med data

---
+++ src/components/SearchComponent.vue
@@ -0,0 +1,17 @@
+const q = query(collection(db, "travel"), where("title", "==", true));
+
+const querySnapshot = (async () => {
+try {
+const searchSnapshot = await getDocs(collection(db, "travel"));
+searchSnapshot.forEach((doc) => {
+return ({id: doc.id, ...doc.data()});
+})
+}
+
+<script>
+
+</script>
+
+<template>
+  <li v-for="result in searchResults" :key="result.id">{{ result.name }}</li>
+</template>
--- src/views/SearchView.vue
+++ src/views/SearchView.vue
@@ -1,37 +1,49 @@
-<script lang="ts">
-import { firebaseApp } from '@/main.ts'
-import {
-  collection,
-  query,
-  where} from "firebase/firestore";
-import {db} from "@/main";
-import {getDocs} from "firebase/firestore/lite";
-import {querySnapshot} from "@firebase/firestore/dist/firestore/test/util/api_helpers";
-import {defineComponent} from "vue";
-
-export default defineComponent({
-  methods: {querySnapshot}
-})
-
-
-const travelCollectionRef = collection(db, "travel")
-
-const q = query(travelCollectionRef, where("title", "==", true));
-
-const querySnapshot = await getDocs(q);
-querySnapshot.forEach((doc) => {
-  // doc.data() is never undefined for query doc snapshots
-  console.log(doc.id, " => ", doc.data());
-});
-</script>
-
 <template>
-  <ion-input type="text" [(ngModel)]="searchQuery" placeholder="Search by title or hashtag"></ion-input>
-  <ion-list>
-    <ion-item *ngFor="let travelItem of travelItems">
-      <ion-label>
-        <h2>{{ querySnapshot.title }}</h2>
-      </ion-label>
-    </ion-item>
-  </ion-list>
+    <ion-header>
+      <ion-toolbar>
+        <ion-title>Search Page</ion-title>
+      </ion-toolbar>
+    </ion-header>
+    <ion-content>
+      <div>
+        <input v-model="searchTerm" />
+        <button @click="search">Search</button>
+        <div v-if="searchResults.length">
+          <ul>
+            <li v-for="result in searchResults" :key="result.id">
+              <div v-for ="(value, field) in result">
+               {{ field }}:{{ value }}
+              </div>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </ion-content>
 </template>
+
+<script lang="ts">import { db } from "@/main";
+import { collection, query, where, getDocs } from "firebase/firestore"
+
+export const getTravels = async (searchTerm: string) => {
+  const q = query(collection(db, 'travel'), where('title', '==', searchTerm));
+  const querySnapshot = await getDocs(q);
+  return querySnapshot.docs.map((doc) => doc.data());
+};
+
+export default {
+  data() {
+    return {
+      searchTerm: '',
+      searchResults: [] as any[],
+    };
+  },
+  methods: {
+    async search() {
+      this.searchResults = await getTravels(this.searchTerm);
+    },
+  },
+};
+
+
+
+</script>
